3ba9300a4761ef386de95f01f346f2313befd765
anumhosen <anumhosen@gmail.com>
2025-11-11 11:45:21 +0600

    vercel configure update

--- config/db.js
+++ config/db.js
@@ -1,12 +1,17 @@
 const mongoose = require('mongoose');
 const connectDB = async () => {
+    if (mongoose.connection.readyState >= 1) return; // Already connected
     try {
-        const connection = await mongoose.connect(process.env.MONGO_URL);
+        const connection = await mongoose.connect(process.env.MONGO_URL, {
+            maxPoolSize: 10,
+            bufferCommands: true,
+        });
         if (connection) {
             console.log('Database connected successfully');
         }
     } catch (e) {
         console.log('Error connecting to database', e);
+        process.exit(1);
     }
 };
 module.exports = connectDB;
